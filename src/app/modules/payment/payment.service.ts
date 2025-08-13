0b5fe926ae4cbda5d78a93f1889e9529ee363c61
Abdullah Al Noman <abdullahalnoman@Abdullahs-Mac-mini.local>
2025-08-13 12:43:53 +0600

    Add validation from ssl

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import config from "../../../config";
 import { prisma } from "../../../shared/prisma";
 import { SSlService } from "../SSL/SSL.service";
-
+import { PaymentStatus } from "../../../generated/prisma";
 
 const initPayment = async (appointmentId: string) => {
   const paymentData = await prisma.payment.findFirstOrThrow({
@@ -18,28 +18,45 @@ const initPayment = async (appointmentId: string) => {
     },
   });
 
-
   const paymentInformation = {
-    amount : paymentData.amount,
+    amount: paymentData.amount,
     transactionId: paymentData.transactionId,
-    name : paymentData.appointment.patient.name,
-    email : paymentData.appointment.patient.email,
-    address : paymentData.appointment.patient.address,
+    name: paymentData.appointment.patient.name,
+    email: paymentData.appointment.patient.email,
+    address: paymentData.appointment.patient.address,
     contactNumber: paymentData.appointment.patient.contactNumber,
-  }
+  };
 
-  const result = await SSlService.initPayment(paymentInformation)
+  const result = await SSlService.initPayment(paymentInformation);
 
   return {
     paymentUrl: result.GatewayPageURL,
-  }
+  };
 };
 
-const validatePayment = async(payload : any) =>{
-    console.log(payload)
-}
+const validatePayment = async (payload: any) => {
+  const response = await SSlService.validatePayment(payload);
+
+  if (response.status !== "VALID") {
+    return {
+      message: "Payment validation failed",
+    };
+  }
+
+  await prisma.$transaction(async (tx) => {
+    await tx.payment.updateMany({
+      where: {
+        transactionId: response.tran_id,
+      },
+      data: {
+        status: PaymentStatus.PAID,
+        paymentGetWayData: response,
+      },
+    });
+  });
+};
 
 export const paymentServices = {
   initPayment,
-  validatePayment
+  validatePayment,
 };
